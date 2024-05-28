fc09a882787a6f1ed67051afcd4a880b40e9b157
Guilherme Silva <guizaomtz@gmail.com>
2024-05-28 18:00:44 -0300

    etapa 1 do tutorial

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using loja.models;
+using loja.data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<LojaDbContext>(options=>options.UseMySql(connectionString, new MySqlServerVersion(new Version(8,0,36))));
 
 
 var app = builder.Build();
---
+++ models/Produto.cs
@@ -0,0 +1,9 @@
+namespace loja.models
+{
+    public class Produto
+    {
+        public int Id{get;set;}
+        public String Nome{get;set;}
+        public Double Preco{get;set;}
+    }
+}
