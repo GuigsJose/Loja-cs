e5bcd0c7923767252611885ffee11fee41737806
Guilherme Silva <guizaomtz@gmail.com>
2024-05-29 10:53:56 -0300

    Etapa 2

--- models/Produto.cs
+++ models/Produto.cs
@@ -5,5 +5,6 @@ namespace loja.models
         public int Id{get;set;}
         public String Nome{get;set;}
         public Double Preco{get;set;}
+        public String Fornecedor{get;set;}
     }
 }
