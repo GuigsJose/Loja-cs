8756696bb71470bb7f53884068498f6b03ca6ec1
Guilherme Silva <guizaomtz@gmail.com>
2024-05-29 20:22:34 -0300

    comments

--- models/Cliente.cs
+++ models/Cliente.cs
@@ -4,6 +4,7 @@ namespace loja.models
 {
     public class Cliente
     {
+        //para criar uma chave primaria, basta utilizar [Key]
         [Key]
         public int Id{get;set;}
         public String Nome{get;set;}
--- models/Produto.cs
+++ models/Produto.cs
@@ -4,6 +4,7 @@ namespace loja.models
 {
     public class Produto
     {
+        //para criar uma chave primaria, basta utilizar [Key]
         [Key]
         public int Id{get;set;}
         public String Nome{get;set;}
